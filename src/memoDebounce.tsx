16306a08b572b507dc6ab50b72bea72f1da76efa
Krystian Sztadhaus <sztadii@gmail.com>
2020-09-03 12:23:58 +0400

    Rename memoDebounce arguments

--- src/memoDebounce.tsx
+++ src/memoDebounce.tsx
@@ -7,15 +7,17 @@ export type CompareFunction<T> = (prevProps?: T, nextProps?: T) => boolean
 export default function memoDebounce<T>(
   ComponentToRender: React.ComponentType<T>,
   delay = 500,
-  isEqualFunction?: CompareFunction<T>
+  isCustomEqualFunction?: CompareFunction<T>
 ) {
   return class DebouncedContainer extends Component<T> {
     prevProps: T | undefined
 
     updateDebounced = debounce((nextProps) => {
-      const functionToCompareProps = isEqualFunction || isDeepEqual
+      const functionToCompareProps = isCustomEqualFunction || isDeepEqual
       const isPropsEqual = functionToCompareProps(nextProps, this.prevProps)
+
       if (isPropsEqual) return
+
       this.savePrevProps()
       this.forceUpdate()
     }, delay)
