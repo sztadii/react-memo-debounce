c542c947d0d709ea95e6a774d1939cbba7b3fc59
Krystian Sztadhaus <sztadii@gmail.com>
2021-07-07 20:58:57 +0200

    Move getInitialElements fn to the bottom

--- src/memoDebounce.test.tsx
+++ src/memoDebounce.test.tsx
@@ -2,6 +2,150 @@ import React, { useState } from 'react'
 import { render } from '@testing-library/react'
 import memoDebounce from './memoDebounce'
 
+describe('memoDebounce', () => {
+  it("render the component's content without any issues", () => {
+    const ParentComponent = getInitialElements(500)
+    const { getByText } = render(<ParentComponent title="Simple title" />)
+
+    getByText('Simple title')
+    getByText('In children childrenRenderCount value 1')
+  })
+
+  it("render the component's updated content with some delay", async () => {
+    const ParentComponent = getInitialElements(1000)
+    const { getByText } = render(<ParentComponent title="Simple title" />)
+
+    getByText('Simple title')
+    getByText('In children childrenRenderCount value 1')
+
+    getByText('Increment parentRenderCount').click()
+
+    await wait(500)
+
+    getByText('In children childrenRenderCount value 1')
+
+    await wait(500)
+
+    getByText('In children childrenRenderCount value 2')
+  })
+
+  it('will not render if updated parent props have the same value', async () => {
+    const ParentComponent = getInitialElements(1000)
+    const { getByText } = render(<ParentComponent title="Simple title" />)
+
+    getByText('Simple title')
+    getByText('In children childrenRenderCount value 1')
+
+    getByText('Toggle parent array value').click()
+
+    await wait(500)
+
+    getByText('In children childrenRenderCount value 1')
+
+    // Back to the first value of the array
+    getByText('Toggle parent array value').click()
+
+    await wait(1100)
+
+    getByText('In children childrenRenderCount value 1')
+  })
+
+  it("when each time parent's props change the deep value then children render the update value", async () => {
+    const ParentComponent = getInitialElements(1000)
+    const { getByText } = render(<ParentComponent title="Simple title" />)
+
+    getByText('Simple title')
+    getByText('In children childrenRenderCount value 1')
+
+    getByText('Toggle parent array value').click()
+    await wait(1000)
+    getByText('In children childrenRenderCount value 2')
+
+    getByText('Toggle parent array value').click()
+    await wait(1000)
+    getByText('In children childrenRenderCount value 3')
+  })
+
+  it('when multi times parent component was updating then children will render only last time', async () => {
+    const ParentComponent = getInitialElements(1000)
+    const { getByText } = render(<ParentComponent title="Simple title" />)
+
+    getByText('Simple title')
+
+    getByText('Increment parentRenderCount').click()
+    getByText('In parent parentRenderCount 1')
+
+    await wait(500)
+    getByText('In children prentRenderCount value 0')
+
+    getByText('Increment parentRenderCount').click()
+    getByText('In parent parentRenderCount 2')
+
+    await wait(500)
+    getByText('In children prentRenderCount value 0')
+
+    getByText('Increment parentRenderCount').click()
+    getByText('In parent parentRenderCount 3')
+
+    await wait(500)
+    getByText('In children prentRenderCount value 0')
+
+    getByText('Increment parentRenderCount').click()
+    getByText('In parent parentRenderCount 4')
+
+    await wait(500)
+    getByText('In children prentRenderCount value 0')
+
+    await wait(500)
+    getByText('In children prentRenderCount value 4')
+    getByText('In parent parentRenderCount 4')
+  })
+
+  it('when isEqualFunction return true then children component never re-render', async () => {
+    const isEqualFunction = () => {
+      return true
+    }
+
+    const ParentComponent = getInitialElements(400, isEqualFunction)
+    const { getByText } = render(<ParentComponent title="Simple title" />)
+
+    getByText('Simple title')
+    getByText('In children childrenRenderCount value 1')
+
+    getByText('Increment parentRenderCount').click()
+
+    await wait(500)
+    getByText('In children childrenRenderCount value 1')
+
+    getByText('Increment parentRenderCount').click()
+
+    await wait(500)
+    getByText('In children childrenRenderCount value 1')
+  })
+
+  it('when isEqualFunction return false then children component always re-render', async () => {
+    const isEqualFunction = () => {
+      return false
+    }
+
+    const ParentComponent = getInitialElements(400, isEqualFunction)
+    const { getByText } = render(<ParentComponent title="Simple title" />)
+
+    getByText('Simple title')
+    getByText('In children childrenRenderCount value 1')
+
+    getByText('Increment parentRenderCount').click()
+
+    await wait(500)
+    getByText('In children childrenRenderCount value 2')
+
+    getByText('Increment parentRenderCount').click()
+
+    await wait(500)
+    getByText('In children childrenRenderCount value 3')
+  })
+})
+
 function wait(delay: number) {
   return new Promise((resolve) => {
     setTimeout(resolve, delay)
@@ -81,151 +225,5 @@ function getInitialElements(
     )
   }
 
-  return {
-    ParentComponent
-  }
+  return ParentComponent
 }
-
-describe('memoDebounce', () => {
-  it("render the component's content without any issues", () => {
-    const { ParentComponent } = getInitialElements(500)
-    const { getByText } = render(<ParentComponent title="Simple title" />)
-
-    getByText('Simple title')
-    getByText('In children childrenRenderCount value 1')
-  })
-
-  it("render the component's updated content with some delay", async () => {
-    const { ParentComponent } = getInitialElements(1000)
-    const { getByText } = render(<ParentComponent title="Simple title" />)
-
-    getByText('Simple title')
-    getByText('In children childrenRenderCount value 1')
-
-    getByText('Increment parentRenderCount').click()
-
-    await wait(500)
-
-    getByText('In children childrenRenderCount value 1')
-
-    await wait(500)
-
-    getByText('In children childrenRenderCount value 2')
-  })
-
-  it('will not render if updated parent props have the same value', async () => {
-    const { ParentComponent } = getInitialElements(1000)
-    const { getByText } = render(<ParentComponent title="Simple title" />)
-
-    getByText('Simple title')
-    getByText('In children childrenRenderCount value 1')
-
-    getByText('Toggle parent array value').click()
-
-    await wait(500)
-
-    getByText('In children childrenRenderCount value 1')
-
-    // Back to the first value of the array
-    getByText('Toggle parent array value').click()
-
-    await wait(1100)
-
-    getByText('In children childrenRenderCount value 1')
-  })
-
-  it("when each time parent's props change the deep value then children render the update value", async () => {
-    const { ParentComponent } = getInitialElements(1000)
-    const { getByText } = render(<ParentComponent title="Simple title" />)
-
-    getByText('Simple title')
-    getByText('In children childrenRenderCount value 1')
-
-    getByText('Toggle parent array value').click()
-    await wait(1000)
-    getByText('In children childrenRenderCount value 2')
-
-    getByText('Toggle parent array value').click()
-    await wait(1000)
-    getByText('In children childrenRenderCount value 3')
-  })
-
-  it('when multi times parent component was updating then children will render only last time', async () => {
-    const { ParentComponent } = getInitialElements(1000)
-    const { getByText } = render(<ParentComponent title="Simple title" />)
-
-    getByText('Simple title')
-
-    getByText('Increment parentRenderCount').click()
-    getByText('In parent parentRenderCount 1')
-
-    await wait(500)
-    getByText('In children prentRenderCount value 0')
-
-    getByText('Increment parentRenderCount').click()
-    getByText('In parent parentRenderCount 2')
-
-    await wait(500)
-    getByText('In children prentRenderCount value 0')
-
-    getByText('Increment parentRenderCount').click()
-    getByText('In parent parentRenderCount 3')
-
-    await wait(500)
-    getByText('In children prentRenderCount value 0')
-
-    getByText('Increment parentRenderCount').click()
-    getByText('In parent parentRenderCount 4')
-
-    await wait(500)
-    getByText('In children prentRenderCount value 0')
-
-    await wait(500)
-    getByText('In children prentRenderCount value 4')
-    getByText('In parent parentRenderCount 4')
-  })
-
-  it('when isEqualFunction return true then children component never re-render', async () => {
-    const isEqualFunction = () => {
-      return true
-    }
-
-    const { ParentComponent } = getInitialElements(400, isEqualFunction)
-    const { getByText } = render(<ParentComponent title="Simple title" />)
-
-    getByText('Simple title')
-    getByText('In children childrenRenderCount value 1')
-
-    getByText('Increment parentRenderCount').click()
-
-    await wait(500)
-    getByText('In children childrenRenderCount value 1')
-
-    getByText('Increment parentRenderCount').click()
-
-    await wait(500)
-    getByText('In children childrenRenderCount value 1')
-  })
-
-  it('when isEqualFunction return false then children component always re-render', async () => {
-    const isEqualFunction = () => {
-      return false
-    }
-
-    const { ParentComponent } = getInitialElements(400, isEqualFunction)
-    const { getByText } = render(<ParentComponent title="Simple title" />)
-
-    getByText('Simple title')
-    getByText('In children childrenRenderCount value 1')
-
-    getByText('Increment parentRenderCount').click()
-
-    await wait(500)
-    getByText('In children childrenRenderCount value 2')
-
-    getByText('Increment parentRenderCount').click()
-
-    await wait(500)
-    getByText('In children childrenRenderCount value 3')
-  })
-})
